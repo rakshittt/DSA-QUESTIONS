29bfca4b9bcbda567e09f94c81ca1ad50112061e
rakshittt <itsrakshitsjain@gmail.com>
2022-10-05 12:35:34 +0530

    Delete code

--- Heap/intro.cpp
+++ Heap/intro.cpp
@@ -44,6 +44,35 @@ public:
         }
         cout << endl;
     }
+
+    void deleteFromheap(){
+        if(size==0){
+            cout<<"Nothing to delete"<<endl;
+            return ;
+        }
+        arr[1]=arr[size];
+        size--;
+
+        // take root node to the correct position
+        int i=1;
+        while(i<size){
+            int leftindex =2*i;
+            int rightindex = 2*i+1;
+
+            if(leftindex <size && arr[i] < arr[leftindex])
+            {
+                swap(arr[i],arr[leftindex]);
+                i=leftindex;
+            }
+            else if(rightindex < size && arr[i] < arr[rightindex]){
+                 swap(arr[i],arr[rightindex]);
+                   i=rightindex;
+            }
+            else{
+                return;
+            }
+        }
+    }
 };
 
 int main()
@@ -55,5 +84,8 @@ int main()
     h.insert(60);
     h.insert(70);
 
+    h.print();
+
+    h.deleteFromheap();
     h.print();
 }
